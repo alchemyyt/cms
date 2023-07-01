c8e48c66e3911b3e94a83239975e7f8af3aa6cb9
jose <josemontesdeoca00@gmail.com>
2023-07-01 19:02:24 -0400

    add togle

---
+++ public/scripts/admin.js
@@ -0,0 +1,26 @@
+const menuButton=document.getElementById('menuIcon');
+const menu = document.getElementsByClassName('menu')[0];
+const menuClose=document.getElementById('menuClose');
+menuButton.addEventListener('click',()=>{
+  active(menu)
+})
+menuClose.addEventListener('click',()=>{
+  active(menu)
+  console.log('click')
+})
+const active=(element)=>{
+  let isElementClose = element.classList.contains('inactive');
+  if (isElementClose === true) {
+      element.classList.remove("inactive");
+      console.log('desactivado')
+  }else{
+    element.classList.add('inactive')
+    console.log('activado')
+  }
+}
+/*else if (isElementClose === true) {
+  active(element)
+}
+const active=(element)=>{
+  element.classList.remove('inactive')
+}*/
