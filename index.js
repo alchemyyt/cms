6f90970c881905054885a2d78e3fcb5182d94edb
jose <josemontesdeoca00@gmail.com>
2023-07-01 00:30:15 -0400

    production prepare

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const routes = require('./routes');
-const port = 3001;
+const port = process.env.PORT||3000;
 app.use(express.static('public'))
 app.set('view engine','pug')
 app.use('/',routes())
@@ -10,3 +10,4 @@ app.set('views',path.join(__dirname,'./views'))
 app.listen(port,()=>{
   console.log('mi port ' + port)
 })
+module.export=app
